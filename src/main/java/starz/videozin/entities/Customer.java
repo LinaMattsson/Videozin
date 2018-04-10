59238bf6f792d02229c4a9183dd40ffa169a24cd
Juulis <Danny.p@Live.se>
2018-04-10 14:03:55 +0200

    First

---
+++ src/main/java/starz/videozin/entities/Customer.java
@@ -0,0 +1,98 @@
+package starz.videozin.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    String socialSecurity;
+    @Column(nullable=false)
+    String fName;
+    @Column(nullable=false)
+    String lName;
+    @Column(nullable=false)
+    String adress;
+    @Column(nullable=false)
+    String town;
+    @Column(nullable=false)
+    String city;
+    @Column(nullable=false)
+    String zip;
+    @Column(nullable=false)
+    String phone;
+    String mail;
+
+    public String getSocialSecurity() {
+        return socialSecurity;
+    }
+
+    public void setSocialSecurity(String socialSecurity) {
+        this.socialSecurity = socialSecurity;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
---
+++ src/main/java/starz/videozin/entities/Movie.java
@@ -0,0 +1,15 @@
+package starz.videozin.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    String pid;
+    @Column(nullable=false)
+    String name;
+    @Column(nullable=false)
+    String category;
+}
