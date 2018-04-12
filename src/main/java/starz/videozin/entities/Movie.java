4fe12ebf71c271364ea2ff3b5556a24a9fd24398
Juulis <Danny.p@Live.se>
2018-04-12 14:00:51 +0200

    fixed Movie Entity

--- src/main/java/starz/videozin/entities/Movie.java
+++ src/main/java/starz/videozin/entities/Movie.java
@@ -12,4 +12,47 @@ public class Movie {
     String name;
     @Column(nullable=false)
     String category;
+    @Column(nullable=false)
+    String format;
+    String description;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
