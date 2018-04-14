418e3d2ab1a66762d08983232f92ad836ac20ab9
Juulis <danny.p@live.se>
2018-04-14 21:31:54 +0200

    fixed some errors

--- src/main/java/starz/videozin/VideozinApplication.java
+++ src/main/java/starz/videozin/VideozinApplication.java
@@ -1,20 +1,12 @@
 package starz.videozin;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VideozinApplication extends SpringBootServletInitializer {
+public class VideozinApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(VideozinApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VideozinApplication.class, args);
+    }
 }
