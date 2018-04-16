460e79385ad426ab459c45961b9905f200ddb287
LinaMattsson <linamattsson@live.se>
2018-04-16 14:47:07 +0200

    inget viktigt

--- src/main/java/starz/videozin/controllers/CustomerController.java
+++ src/main/java/starz/videozin/controllers/CustomerController.java
@@ -1,25 +1,53 @@
 package starz.videozin.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import starz.videozin.entities.Customer;
+import starz.videozin.repositories.CustomerRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
 public class CustomerController {
-    @GetMapping("/hi")
-    public String sayhi(){
-        return "hi";
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping("/user")
+    public String addUser(Model model){
+        model.addAttribute("customer", new Customer());
+        return "userpage";
     }
 
+    @PostMapping("/user")
+    public String seNewUser(@ModelAttribute Customer customer){
+
+       return "testtest";
+    }
     @GetMapping("")
     public String start(){
-        return "hellotest";
+        return "testtest";
     }
-    @PostMapping("")
+    @GetMapping("/User")
     public String start1(){
-        return "hellotest";
+        return "user page";
     }
+//    @GetMapping("/addUser")
+//    public String addUser(Model model,){
+//        return "user";
+//    }
+//    @GetMapping("/Users")
+//    public String getCustomer(Model model){
+//        model.addAttribute()
+//        return "user"
+//    }
 }
