25f3ddd7bdf21fb9835d428e7d9ee9fc55d1acc7
Linn <linn.martensson@hotmail.se>
2018-04-24 12:32:57 +0200

    fixed error regarding style not being available before login

--- src/main/java/starz/videozin/configuration/WebSecurityConfiguration.java
+++ src/main/java/starz/videozin/configuration/WebSecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/***").permitAll();
         http.formLogin()
                 .loginPage("/")
                 .defaultSuccessUrl("/start")
